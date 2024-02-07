ee416927efc83af6af3893004f043415df444bf3
Colter Snyder <programmer114@gmail.com>
2024-02-07 15:46:21 -0700

    Initial Commit

---
+++ App2_Grades_inClass/App2_Grades_inClassApp.swift
@@ -0,0 +1,17 @@
+//
+//  App2_Grades_inClassApp.swift
+//  App2_Grades_inClass
+//
+//  Created by Colter Snyder on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct App2_Grades_inClassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App2_Grades_inClass/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App2_Grades_inClass
+//
+//  Created by Colter Snyder on 2/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
